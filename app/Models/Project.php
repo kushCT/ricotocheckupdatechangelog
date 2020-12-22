03e63d00fba4abda2b35f1b62bcf5583765d13b6
Hassane Dao <dao.hassane@gmail.com>
2020-12-22 16:52:58 +0000

    chore(): rico php package

---
+++ app/Models/Project.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Jetstream\HasTeams;
+
+class Project extends Model
+{
+    use HasFactory, HasTeams;
+}
