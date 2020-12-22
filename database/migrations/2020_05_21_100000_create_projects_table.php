26c2d400f9b5a53477eee001b2e05530c397ba60
Hassane Dao <dao.hassane@gmail.com>
2020-12-22 16:20:16 +0000

    chore(): migrations

--- database/migrations/2020_05_21_100000_create_projects_table.php
+++ database/migrations/2020_05_21_100000_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTeamUserTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateTeamUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('team_user', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('team_id');
-            $table->foreignId('user_id');
-            $table->string('role')->nullable();
+            $table->foreignId('user_id')->index();
+            $table->string('name');
+            $table->boolean('personal_project');
             $table->timestamps();
-
-            $table->unique(['team_id', 'user_id']);
         });
     }
 
@@ -31,6 +29,6 @@ class CreateTeamUserTable extends Migration
      */
     public function down()
     {
-        Schema::drop('team_user');
+        Schema::drop('projects');
     }
 }
