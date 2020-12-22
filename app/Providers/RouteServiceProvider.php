cd7a0852927d2bc6cf9de81cd20c3d90a8fef632
Hassane Dao <dao.hassane@gmail.com>
2020-12-22 16:18:04 +0000

    chore(): route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,10 +38,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api/1.0')
                 ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->namespace('App\\Http\\Controllers\\Api\\V1')
+                ->group(base_path('routes/apis/1.0.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
