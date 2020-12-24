208731b6607a3f9de3c69ed9993f42de3672c23d
houssenedao <dao.houssene@gmail.com>
2020-12-24 12:11:36 +0000

    chore(): remove other input

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -1,21 +1,10 @@
 <form wire:submit.prevent="register" class="space-y-6">
     <div>
-        <label for="name" class="block text-sm font-medium text-gray-700">
-            {{ __('Name') }}
-        </label>
-        <div class="mt-1">
-            <input wire:model="name" id="name" name="name" type="text" required
-                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-        </div>
-        <p class="text-xs text-red-500"> @error('name') {{ __($message) }} @enderror</p>
-    </div>
-
-    <div>
         <label for="email" class="block text-sm font-medium text-gray-700">
             {{ __('Email address') }}
         </label>
         <div class="mt-1">
-            <input wire:model="email" id="email" name="email" type="email" autocomplete="email" required
+            <input wire:model="email" id="email" name="email" type="email" autocomplete="email" required placeholder="your.email@email.com"
                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
         <p class="text-xs text-red-500"> @error('email') {{ __($message) }} @enderror</p>
@@ -27,22 +16,10 @@
         </label>
         <div class="mt-1">
             <input wire:model="password" id="password" name="password" type="password" autocomplete="current-password"
-                   required
+                   required placeholder="password"
                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
         <p class="text-xs text-red-500"> @error('password') {{ __($message) }} @enderror</p>
-    </div>
-
-    <div class="space-y-1">
-        <label for="passwordConfirmation" class="block text-sm font-medium text-gray-700">
-            {{ __('Password Confirmation') }}
-        </label>
-        <div class="mt-1">
-            <input wire:model.lazy="passwordConfirmation" id="passwordConfirmation" name="passwordConfirmation"
-                   type="password" required
-                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-        </div>
-        <p class="text-xs text-red-500"> @error('passwordConfirmation') {{ __($message) }} @enderror</p>
     </div>
 
     <div>
