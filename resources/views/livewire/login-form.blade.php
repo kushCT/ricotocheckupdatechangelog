6d71f0e9c697914c34d90762f8256b1525546448
Hassane Dao <dao.hassane@gmail.com>
2020-12-24 10:24:23 +0000

    chore(): translation

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -4,7 +4,7 @@
             {{ __('Email address') }}
         </label>
         <div class="mt-1">
-            <input id="email" name="email" type="email" autocomplete="email" required
+            <input wire:model="email" id="email" name="email" type="email" autocomplete="email" required
                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
     </div>
@@ -14,7 +14,7 @@
             {{ __('Password') }}
         </label>
         <div class="mt-1">
-            <input id="password" name="password" type="password" autocomplete="current-password"
+            <input wire:model="password" id="password" name="password" type="password" autocomplete="current-password"
                    required
                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
@@ -28,18 +28,18 @@
                 {{ __('Remember me') }}
             </label>
         </div>
-
-        <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                {{ __('Forgot your password?') }}
-            </a>
-        </div>
     </div>
 
     <div>
         <button type="submit"
-                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-md tracking-wide font-medium text-black bg-yellow-300 hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             {{ __('Login') }}
         </button>
+    </div>
+
+    <div class="text-sm text-center">
+        <a href="{{ route('password.request') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
+            {{ __('Forgot your password?') }}
+        </a>
     </div>
 </form>
