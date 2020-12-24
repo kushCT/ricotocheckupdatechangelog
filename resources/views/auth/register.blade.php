bb5dbaf5db9df20f814def4576fbc4ac87127e84
Hassane Dao <dao.hassane@gmail.com>
2020-12-24 09:00:35 +0000

    chore(): login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,32 @@
 <x-guest-layout>
-    @livewire('register-form')
+    <div class="min-h-screen bg-white flex">
+        <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div class="mx-auto w-full max-w-sm lg:w-96">
+                <div>
+                    <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                         alt="Workflow">
+                    <h2 class="mt-6 text-3xl font-extrabold text-gray-900">
+                        Sign in to your account
+                    </h2>
+                    <p class="mt-2 text-sm text-gray-600 max-w">
+                        Or
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                            start your 14-day free trial
+                        </a>
+                    </p>
+                </div>
+
+                <div class="mt-8">
+                    <div class="mt-6">
+                        @livewire('register-form')
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hidden lg:block relative w-0 flex-1">
+            <img class="absolute inset-0 h-full w-full object-cover"
+                 src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+                 alt="">
+        </div>
+    </div>
 </x-guest-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     purge: [
         './resources/**/*.blade.php',
@@ -6,7 +8,11 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
         screen: {
             'tablet': '640px',
             'laptop': '1024px',
@@ -17,6 +23,8 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        require('@tailwindcss/forms')
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio')
     ],
 }
