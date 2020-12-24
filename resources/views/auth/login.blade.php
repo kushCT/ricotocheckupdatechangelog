b45106cb1739acd6ada75cb66977d7d1de55456a
Hassane Dao <dao.hassane@gmail.com>
2020-12-24 21:15:28 +0000

    chore(): remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,9 +48,9 @@
 
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center">
-                                    <input id="remember_me" name="remember_me" type="checkbox"
+                                    <input id="remember" name="remember" type="checkbox"
                                            class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                                    <label for="remember_me" class="ml-2 block text-sm text-gray-900">
+                                    <label for="remember" class="ml-2 block text-sm text-gray-900">
                                         {{ __('Remember me') }}
                                     </label>
                                 </div>
