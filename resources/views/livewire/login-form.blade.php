0aa5c87483f82a8554703d18f39bf8675b10013c
Hassane Dao <dao.hassane@gmail.com>
2020-12-24 11:50:53 +0000

    chore(): translation

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -5,7 +5,7 @@
         </label>
         <div class="mt-1">
             <input wire:model="email" id="email" name="email" type="email" autocomplete="email" required placeholder="your.name@email.com"
-                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                   class="appearance-none block w-full px-3 py-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
     </div>
 
@@ -16,7 +16,7 @@
         <div class="mt-1">
             <input wire:model="password" id="password" name="password" type="password" autocomplete="current-password"
                    required placeholder="password"
-                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                   class="appearance-none block w-full px-3 py-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </div>
     </div>
 
