fec44e7a9a551c48f00ccd5f94435a31b325728c
houssenedao <dao.houssene@gmail.com>
2020-12-24 21:16:26 +0000

    chore(): update organization policy

--- app/Policies/OrganizationPolicy.php
+++ app/Policies/OrganizationPolicy.php
@@ -13,82 +13,102 @@ class OrganizationPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
+     * @param User $user
+     *
      * @return mixed
      */
     public function viewAny(User $user)
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Organization  $organization
+     * @param User $user
+     * @param Organization $organization
+     *
      * @return mixed
      */
     public function view(User $user, Organization $organization)
     {
-        //
+        return $user->belongsToOrganization($organization);
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
+     * @param User $user
+     *
      * @return mixed
      */
     public function create(User $user)
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Organization  $organization
+     * @param User $user
+     * @param Organization $organization
+     *
      * @return mixed
      */
     public function update(User $user, Organization $organization)
     {
-        //
+        return $user->ownsOrganization($organization);
+    }
+
+    /**
+     * Determine whether the user can add organization members.
+     *
+     * @param User $user
+     * @param Organization $organization
+     *
+     * @return mixed
+     */
+    public function addOrganizationMember(User $user, Organization $organization)
+    {
+        return $user->ownsOrganization($organization);
+    }
+
+    /**
+     * Determine whether the user can update organization member permissions.
+     *
+     * @param User $user
+     * @param Organization $organization
+     *
+     * @return mixed
+     */
+    public function updateOrganizationMember(User $user, Organization $organization)
+    {
+        return $user->ownsOrganization($organization);
+    }
+
+    /**
+     * Determine whether the user can remove organization members.
+     *
+     * @param User $user
+     * @param Organization $organization
+     *
+     * @return mixed
+     */
+    public function removeOrganizationMember(User $user, Organization $organization)
+    {
+        return $user->ownsOrganization($organization);
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Organization  $organization
+     * @param User $user
+     * @param Organization $organization
+     *
      * @return mixed
      */
     public function delete(User $user, Organization $organization)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Organization  $organization
-     * @return mixed
-     */
-    public function restore(User $user, Organization $organization)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Organization  $organization
-     * @return mixed
-     */
-    public function forceDelete(User $user, Organization $organization)
-    {
-        //
+        return $user->ownsOrganization($organization);
     }
 }
