4849c92e5e7b2f2ae6f2fc0099488549e756a77b
houssenedao <dao.houssene@gmail.com>
2020-12-25 12:21:43 +0000

    chore(): switch organization

--- database/migrations/2020_12_23_083809_create_organizations_table.php
+++ database/migrations/2020_12_23_083809_create_organizations_table.php
@@ -23,7 +23,7 @@ class CreateOrganizationsTable extends Migration
             $table->string('name')->unique()
                 ->index();
             $table->string('slug')->unique()->index();
-            $table->boolean('personal_organization')->nullable();
+            $table->boolean('personal_organization')->default(false);
             $table->softDeletes();
             $table->timestamps();
         });
