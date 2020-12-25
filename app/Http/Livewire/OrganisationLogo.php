4884657fb3ec7526d0822f817f40c8958dc5b11d
Hassane Dao <dao.hassane@gmail.com>
2020-12-25 01:28:35 +0000

    chore(): organization livewire

--- app/Http/Livewire/OrganisationLogo.php
+++ app/Http/Livewire/OrganisationLogo.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class OrganisationLogo extends Component
 {
+    /**
+     * @var mixed
+     */
+    public $organization;
+
+    /**
+     * @param $organization
+     */
+    public function mount($organization)
+    {
+        $this->organization = $organization;
+    }
+
+    /**
+     * @return Application|Factory|View
+     */
     public function render()
     {
         return view('livewire.organisation-logo');
