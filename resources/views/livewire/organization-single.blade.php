cadedfc5f2a7f667f20102e029ccfb26157fe2de
houssenedao <dao.houssene@gmail.com>
2020-12-25 17:59:58 +0000

    chore(): switch subscription

--- resources/views/livewire/organization-single.blade.php
+++ resources/views/livewire/organization-single.blade.php
@@ -1,6 +1,6 @@
 <a href="#"
     wire:click.prevent="changeOrganization('{{ $organization->id }}')"
     @if($isCurrentOrganization) disabled @endif
-    class="rounded-full bg-gray-300 w-12 h-12 text-white flex justify-center items-center @if($isCurrentOrganization) cursor-not-allowed @else cursor-pointer @endif">
+    class="rounded-full bg-gray-300 w-12 h-12 text-white flex justify-center items-center @if($isCurrentOrganization) pointer-events-none cursor-not-allowed @else cursor-pointer @endif">
     {!! $organization->nameInitial() !!}
 </a>
