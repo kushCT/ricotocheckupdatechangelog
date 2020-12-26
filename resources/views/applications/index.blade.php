545bcf7d1fe0855058a8faed83e1b8be70938813
Hassane Dao <dao.hassane@gmail.com>
2020-12-26 15:24:24 +0000

    chore(): app layouts

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -130,38 +130,26 @@
         <x-app-sidebar/>
         <!-- Static sidebar for desktop -->
         <div class="hidden lg:flex lg:flex-shrink-0">
-            <div class="flex flex-col w-64 border-r border-gray-200 pt-5 pb-4 bg-gray-100">
-                <div class="flex items-center flex-shrink-0 px-6">
-                    <img class="h-8 w-auto"
-                         src="https://tailwindui.com/img/logos/workflow-logo-purple-500-mark-gray-700-text.svg"
-                         alt="Workflow">
-                </div>
+            <div class="flex flex-col w-64 border-r border-gray-200 pb-4 bg-gray-100">
+
                 <!-- Sidebar component, swap this element with another sidebar if you like -->
                 <div class="h-0 flex-1 flex flex-col overflow-y-auto">
                     <!-- User account dropdown -->
                     <div class="px-3 mt-6 relative inline-block text-left">
                         <!-- Dropdown menu toggle, controlling the show/hide state of dropdown menu. -->
                         <div>
-                            <button type="button"
-                                    class="group w-full bg-gray-100 rounded-md px-3.5 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500"
-                                    id="options-menu" aria-haspopup="true" aria-expanded="true">
+                            <button type="button" class="group w-full bg-gray-100 rounded-md px-3.5 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500" id="options-menu" aria-haspopup="true" aria-expanded="true">
                               <span class="flex w-full justify-between items-center">
                                 <span class="flex min-w-0 items-center justify-between space-x-3">
-                                  <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-                                       src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-                                       alt="">
+                                  <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" alt="">
                                   <span class="flex-1 min-w-0">
                                     <span class="text-gray-900 text-sm font-medium truncate">Jessy Schwarz</span>
                                     <span class="text-gray-500 text-sm truncate">@jessyschwarz</span>
                                   </span>
                                 </span>
                                   <!-- Heroicon name: selector -->
-                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                     aria-hidden="true">
-                                  <path fill-rule="evenodd"
-                                        d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"/>
+                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                  <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/>
                                 </svg>
                               </span>
                             </button>
@@ -205,63 +193,37 @@
                             </div>
                         </div>--}}
                     </div>
-                    <!-- Sidebar Search -->
-                    <div class="px-3 mt-5">
-                        <label for="search" class="sr-only">Search</label>
-                        <div class="mt-1 relative rounded-md shadow-sm">
-                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-                                 aria-hidden="true">
-                                <!-- Heroicon name: search -->
-                                <svg class="mr-3 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                            </div>
-                            <input type="text" name="search" id="search"
-                                   class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-9 sm:text-sm border-gray-300 rounded-md"
-                                   placeholder="Search">
-                        </div>
-                    </div>
                     <!-- Navigation -->
                     <nav class="px-3 mt-6">
                         <div class="space-y-1">
                             <!-- Current: "bg-gray-200 text-gray-900", Default: "text-gray-700 hover:text-gray-900 hover:bg-gray-50" -->
                             <a href="#"
-                               class="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                               class="bg-white shadow-sm text-black mb-2 tracking-wide group flex items-center px-3 py-3 text-md font-light rounded-md">
                                 <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" -->
                                 <!-- Heroicon name: home -->
-                                <svg class="text-gray-500 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                                <svg class="text-gray-700 group-hover:text-gray-500 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                                 </svg>
-                                Home
+                                Mes applications
                             </a>
 
                             <a href="#"
-                               class="text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                               class="text-gray-700 hover:text-gray-900 hover:bg-gray-50 mb-2 tracking-wide group flex items-center px-3 py-3 text-md font-thin rounded-md">
                                 <!-- Heroicon name: view-list -->
-                                <svg class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
-                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
+                                <svg class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
-                                My tasks
+                                Membres
                             </a>
 
                             <a href="#"
-                               class="text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                               class="text-gray-700 hover:text-gray-900 hover:bg-gray-50 mb-2 tracking-wide group flex items-center px-3 py-3 text-md font-thin rounded-md">
                                 <!-- Heroicon name: clock -->
-                                <svg class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
-                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                <svg class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
-                                Recent
+                                Parametres
                             </a>
                         </div>
                         <div class="mt-8">
