a6851fe9b06fe4675dfa380a7b9d8b666a6bfd24
houssenedao <dao.houssene@gmail.com>
2020-12-26 19:34:44 +0000

    chore(): update page

--- resources/views/applications/create.blade.php
+++ resources/views/applications/create.blade.php
@@ -1,6 +1,11 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-black text-2xl leading-tight">
+            {{ __('Create application') }}
+        </h2>
+    </x-slot>
     <div>
-        {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day --}}
+        {{-- Success is as dangerous as failure. --}}
         App create
     </div>
 </x-app-layout>
--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-black text-2xl leading-tight">
+            {{ __('Applications') }}
+        </h2>
+    </x-slot>
     <div>
         {{-- Success is as dangerous as failure. --}}
         App index
