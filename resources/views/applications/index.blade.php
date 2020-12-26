9809fd47e4e415f0709de197ab9d89b253cfeab9
Hassane Dao <dao.hassane@gmail.com>
2020-12-26 13:41:40 +0000

    chore(): app layouts

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-
     <div class="h-screen flex overflow-hidden bg-white">
         <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
         <div class="lg:hidden">
@@ -276,24 +275,24 @@
                                    class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
                                     <span class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full" aria-hidden="true"></span>
                                     <span class="truncate">
-                  Engineering
-                </span>
+                                      Engineering
+                                    </span>
                                 </a>
 
                                 <a href="#"
                                    class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
                                     <span class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full" aria-hidden="true"></span>
                                     <span class="truncate">
-                  Human Resources
-                </span>
+                                      Human Resources
+                                    </span>
                                 </a>
 
                                 <a href="#"
                                    class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
                                     <span class="w-2.5 h-2.5 mr-4 bg-yellow-500 rounded-full" aria-hidden="true"></span>
                                     <span class="truncate">
-                  Customer Success
-                </span>
+                                      Customer Success
+                                    </span>
                                 </a>
                             </div>
                         </div>
