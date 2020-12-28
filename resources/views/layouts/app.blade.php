e625c01d655a1099fad2622686d97cbafdac0b56
houssenedao <dao.houssene@gmail.com>
2020-12-28 15:56:07 +0000

    chore(): content config with slot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,89 +17,15 @@
 <body class="font-sans antialiased">
     <div class="h-screen bg-gray-100 flex">
         <!-- sidebar -->
-        <div class="flex-shrink-0 flex flex-col items-center py-8 justify-between w-20 border-r border-gray-200">
-            <!-- user avatar -->
-            <div>
-                <a href="#" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                </a>
-            </div>
-
-            <!-- main navigation -->
-            <nav class="flex space-y-8 flex flex-col">
-                <a href="#" class="text-white p-3 lg:hover:text-yellow-300 bg-black rounded-md">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 p-3">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 p-3">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 p-3">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 p-3">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                </a>
-            </nav>
-
-            <!-- logout -->
-            <div class="logout flex justify-center items-center">
-                <a href="#" class="text-gray-500">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
-                    </svg>
-                </a>
-            </div>
-        </div>
+        @livewire('sidebar')
 
         <div class="flex-grow flex flex-col">
             <!-- navigation -->
-            <header class="flex-shrink-0 border-b border-gray-200 px-14 py-4 relative flex items-center justify-between">
-                <!-- logo -->
-                <h2 class="logo mr-6">
-                    Rico apps
-                </h2>
-
-                <!-- search input -->
-                <div class="flex-1 flex justify-center px-2 ml-6 lg:justify-start">
-                    <label for="search" class="sr-only">Search</label>
-                    <div class="relative w-2/6">
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                        </div>
-                        <input id="search" name="search" class="block min-w-100 w-full pl-10 pr-10 py-3 border border-transparent rounded-full leading-5 bg-white text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-600 sm:text-sm" placeholder="Search" type="search">
-                    </div>
-                </div>
-
-                <!-- application menu -->
-                <nav class="flex justify-center lg:justify-end space-x-10">
-                    <a href="#" class="text-sm font-medium text-gray-400 lg:hover:text-black">Dashboard</a>
-                    <a href="#" class="text-sm font-medium text-gray-400 lg:hover:text-black">Stats</a>
-                    <a href="#" class="text-sm font-medium text-gray-400 lg:hover:text-black">Clients</a>
-                    <a href="#" class="text-sm font-medium text-gray-400 lg:hover:text-black">Collaborators</a>
-                    <a href="#" class="text-sm font-medium text-gray-400 lg:hover:text-black">Settings</a>
-                </nav>
-            </header>
+            @livewire('navigation')
 
             <!-- content -->
             <main class="flex-grow px-14 pt-6">
-                // Hello
+                {{ $slot }}
             </main>
         </div>
     </div>
