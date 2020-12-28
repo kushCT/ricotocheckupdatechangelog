fc6e1349b018d516efdaeefc34bb32d71ed5967e
houssenedao <dao.houssene@gmail.com>
2020-12-28 15:41:53 +0000

    chore(): search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,11 +79,11 @@
                     <label for="search" class="sr-only">Search</label>
                     <div class="relative w-2/6">
                         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg class="h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                         </div>
-                        <input id="search" name="search" class="block min-w-100 w-full pl-10 pr-10 py-3 border border-transparent rounded-full leading-5 bg-white text-gray-300 placeholder-gray-300 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-600 sm:text-sm" placeholder="Search" type="search">
+                        <input id="search" name="search" class="block min-w-100 w-full pl-10 pr-10 py-3 border border-transparent rounded-full leading-5 bg-white text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-600 sm:text-sm" placeholder="Search" type="search">
                     </div>
                 </div>
 
