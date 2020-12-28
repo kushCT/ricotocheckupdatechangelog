bccc8650d9e361657ce2a5c9eb085067d1f250f8
Hassane Dao <dao.hassane@gmail.com>
2020-12-28 19:04:39 +0000

    chore(): application list

--- resources/views/applications/livewire/app-index.blade.php
+++ resources/views/applications/livewire/app-index.blade.php
@@ -1,8 +1,5 @@
-<div class="py-8">
-    <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Mes applications</h2>
-    <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        @foreach($applications as $app)
-            <livewire:application-single :app="$app" :key="$app->id" />
-        @endforeach
-    </ul>
-</div>
+<ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+    @foreach($applications as $app)
+        <livewire:application-single :app="$app" :key="$app->id" />
+    @endforeach
+</ul>
