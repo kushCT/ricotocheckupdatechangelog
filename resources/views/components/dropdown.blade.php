6f4db99d7c2d85bb28cd6dad01858b12b0bb5a3f
Hassane Dao <dao.hassane@gmail.com>
2020-12-29 11:45:06 +0000

    chore(): application list

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -36,7 +36,7 @@
          class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
          style="display: none;"
          @click="open = false">
-        <div class="rounded-md shadow-xs {{ $contentClasses }}">
+        <div class="rounded-md shadow-xs border {{ $contentClasses }}">
             {{ $content }}
         </div>
     </div>
