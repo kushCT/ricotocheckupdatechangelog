e64a3d3b73b5508fc8cb32de6e5633af50219de2
Hassane Dao <dao.hassane@gmail.com>
2020-12-29 09:18:12 +0000

    chore(): application list

--- resources/views/applications/livewire/app-single.blade.php
+++ resources/views/applications/livewire/app-single.blade.php
@@ -28,12 +28,15 @@
     <div class="flex-1 flex truncate">
         <div class="flex-1 px-2 truncate">
             <a href="#" class="text-gray-900 text-lg font-medium hover:text-gray-600">{{ $app->name }}</a>
-            <p class="text-gray-500 text-xs">{{ $app->created_at->format('d-m-Y H:i:s') }}</p>
-            <div class="flex -space-x-2 relative z-0 overflow-hidden pt-3 pb-2">
-                <img class="relative z-30 inline-block h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                <img class="relative z-20 inline-block h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                <img class="relative z-10 inline-block h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="">
-                <img class="relative z-0 inline-block h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            <p class="text-gray-500 text-xs">{{ $app->created_at->diffForHumans() }}</p>
+            <div class="flex items-center space-x-2 pt-3 pb-2">
+                <div class="flex flex-shrink-0 -space-x-2">
+                    <img class="max-w-none h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <img class="max-w-none h-8 w-8 rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <img class="max-w-none h-8 w-8 rounded-full rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="">
+                    <img class="max-w-none h-8 w-8 rounded-full rounded-full border-2 border-white shadow" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                </div>
+                <span class="flex-shrink-0 text-xs leading-5 text-gray-300 font-medium">+8</span>
             </div>
         </div>
         <div class="flex-shrink-0 pr-2">
