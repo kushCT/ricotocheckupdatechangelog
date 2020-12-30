d396efd3928e74e731212c32b3f10c60548deecc
Hassane Dao <dao.hassane@gmail.com>
2020-12-30 10:19:08 +0000

    chore(): pinned applications action

--- app/Http/Livewire/PinnedApplication.php
+++ app/Http/Livewire/PinnedApplication.php
@@ -24,6 +24,57 @@ class PinnedApplication extends Component
         $this->app = $app;
     }
 
+    /**
+     * Set status.
+     *
+     * @param string $status
+     * @throws AuthorizationException
+     */
+    public function setStatus(string $status)
+    {
+        $this->resetErrorBag();
+
+        if ($status === 'online') {
+            (new ApplicationAction())
+                ->online($this->app, request()->user());
+        } else {
+            (new ApplicationAction())
+                ->paused($this->app, request()->user());
+        }
+    }
+
+    /**
+     * @throws AuthorizationException
+     */
+    public function pinned()
+    {
+        $this->resetErrorBag();
+
+        if ($this->app->isPinned()) {
+            (new ApplicationAction())->unpinned($this->app, request()->user());
+        } else {
+            (new ApplicationAction())->pinned($this->app, request()->user());
+        }
+
+        $this->emit('applicationPinned');
+    }
+
+    /**
+     * @throws AuthorizationException
+     */
+    public function archived()
+    {
+        $this->resetErrorBag();
+
+        if ($this->app->isArchived()) {
+            (new ApplicationAction())->unarchived($this->app, request()->user());
+        } else {
+            (new ApplicationAction())->archived($this->app, request()->user());
+        }
+
+        $this->emit('applicationArchived');
+    }
+
     public function render()
     {
         return view('applications.livewire.pinned-app');
--- resources/views/applications/livewire/archived-app.blade.php
+++ resources/views/applications/livewire/archived-app.blade.php
@@ -3,7 +3,7 @@
 
     <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
         @foreach($applications as $app)
-            <livewire:application-single :app="$app" :key="$app->id" />
+            <livewire:application-single :app="$app" :key="'archived_'.$app->id" />
         @endforeach
     </ul>
 </div>
