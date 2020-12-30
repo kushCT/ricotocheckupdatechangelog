44bf561c8346b119f9260d401901b6167c066d86
Hassane Dao <dao.hassane@gmail.com>
2020-12-30 15:36:21 +0000

    chore(): pinned applications action

--- resources/views/applications/livewire/archived-app.blade.php
+++ resources/views/applications/livewire/archived-app.blade.php
@@ -13,8 +13,9 @@
             <svg class="w-20 h-20 opacity-5 mb-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
             </svg>
-            <p class="font-light text-2xl text-gray-300 tracking-wide">
-                {{ __('Nothing app is archived') }}
+            <h3 class="font-bold text-xl opacity-30">{{ __('Archived applications') }}</h3>
+            <p class="font-light text-lg text-gray-300 tracking-wide">
+                {{ __('You have no archived applications') }}
             </p>
         </div>
     @endif
